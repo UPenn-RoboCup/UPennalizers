ddb3da97d383e720a9152d157f2974831effe6f2
brindza <brindza@tabor.(none)>
2011-09-12 17:17:22 -0400

    added Config_Webots_Nao_Vision for webots specific vision parameters

---
+++ Player/Config/Config_Webots_Nao_Vision.lua
@@ -0,0 +1,45 @@
+module(..., package.seeall);
+
+require('vector')
+
+-- Vision Parameters
+
+color = {};
+color.orange = 1;
+color.yellow = 2;
+color.cyan = 4;
+color.field = 8;
+color.white = 16;
+
+vision = {};
+vision.maxFPS = 30;
+vision.scaleB = 4;
+vision.ball_diameter = 0.065;
+vision.ball_height_max = -0.20;
+vision.yellow_goal_count_thres = 150;
+
+-- use this to enable line detection
+vision.enable_line_detection = 0;
+-- use this to enable spot detection
+vision.enable_spot_detection = 0;
+-- use this to enable midfield landmark detection
+vision.enable_midfield_landmark_detection = 0;
+-- Enable the Velocity filter
+vision.enable_velocity_detection = 0;
+-- use this to enable copying images to shm (for colortables, testing)
+vision.copy_image_to_shm = 0;
+-- use this to enable storing all images
+vision.store_all_images = 0;
+-- use this to enable storing images where the goal was detected
+vision.store_goal_detections = 0;
+-- use this to enable storing images where the ball was detected
+vision.store_ball_detections = 0;
+-- use this to enable ground check 
+vision.check_for_ground = 0;
+
+-- use this to substitute goal check with blue/yellow ball check
+vision.use_point_goal = 0;
+
+vision.ballColor = color.orange;
+vision.goal1Color = color.yellow;
+vision.goal2Color = color.cyan;
