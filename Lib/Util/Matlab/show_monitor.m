8dd8549d833ca68c57a892a9873af63371a2546a
Stephen McGill <smcgill3@seas.upenn.edu>
2011-12-08 16:39:56 -0500

    Track mulitple robots correctly in the monitor.

--- Lib/Util/Matlab/show_monitor.m
+++ Lib/Util/Matlab/show_monitor.m
@@ -13,8 +13,8 @@ else
 end
 rgb = robots{playerNumber,teamNumber}.get_rgb();
 
-teamNumbers = size(robots,1);
-nPlayers = size(robots,2);
+teamNumbers = size(robots,2);
+nPlayers = size(robots,1);
 
 % Colormap
 cbk=[0 0 0];cr=[1 0 0];cg=[0 1 0];cb=[0 0 1];cy=[1 1 0];cw=[1 1 1];
