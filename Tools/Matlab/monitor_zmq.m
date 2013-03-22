d69125de42241dbaa608c952be7723d2260abe54
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-03-22 00:04:30 -0400

    Fast monitor with a reasonable timeout on receiving messages

--- Tools/Matlab/monitor_zmq.m
+++ Tools/Matlab/monitor_zmq.m
@@ -18,39 +18,65 @@ s.connect( channel );
 mask=java.lang.String('a'); % labelA
 s.subscribe( mask.getBytes() );
 mask=java.lang.String('i'); % image
+% TODO: set mask to use the id of the player we wish to track
 s.subscribe( mask.getBytes() );
-s.setReceiveTimeOut(1);
+s.setReceiveTimeOut(1e3); % 1 second timeout, timeout is in ms
+% TODO: set high water mark.
 %%
 cbk=[0 0 0];cr=[1 0 0];cg=[0 1 0];cb=[0 0 1];cy=[1 1 0];cw=[1 1 1];
 cmap=[cbk;cr;cy;cy;cb;cb;cb;cb;cg;cg;cg;cg;cg;cg;cg;cg;cw];
-figure(1);
+f = figure(1);
 % Speed: http://www.mathworks.com/support/solutions/en/data/1-1B022/?solution=1-1B022
 set(gcf,'doublebuffer','off');
 subplot(1,2,1);
-h = image( zeros(160,120,3) );
+pc = gca;
+hc = image( zeros(160,120,3) );
 xlim([1 160]);
 ylim([1 120]);
 subplot(1,2,2);
+pl = gca;
 colormap(cmap);
-h2 = image( uint8(zeros(160,120)) );
+hl = image( uint8(zeros(160,120)) );
 xlim([1 160]);
 ylim([1 120]);
+t0 = tic;
+tl = tic;
+tc = tic;
+fps_c = 0;
+fps_l = 0;
+target_fps = 16;
+inv_target_fps = 1/target_fps;
+dirty = 0;
+counter = uint32(0);
 while 1
     q = s.recv();
     if(~isempty(q))
+        dirty = 1;
         myt = q(1);
         %disp(myt);
         qq = q(2:end);
         if myt~=97
-            %disp('got img');
+            %fps_c = (1/toc(tc))*.8+.2*fps_c;
+            %tc = tic;
             img = djpeg(qq);
-            set(h,'Cdata',img);
+            set(hc,'Cdata',img);
+            %title(pc, sprintf('FPS: %.1f',fps_c) );
         else
-            %disp('got label');
+            %fps_l = (1/toc(tl))*.8+.2*fps_l;
+            %tl = tic;
             rawu8 = typecast(qq, 'uint8');
             label = reshape(rawu8(1:160*120), [160, 120])';
-            set(h2,'Cdata',label);
+            set(hl,'Cdata',label);
+            %title(pl, sprintf('FPS: %.1f',fps_l) );
         end
+        % Control the drawing
+        t_diff = toc(t0);
+        if t_diff>inv_target_fps && dirty==1
+            title(pc, sprintf('FPS: %.1f\tCounter: %ld',1/t_diff, counter) );
+            dirty = 0;
+            drawnow;
+            t0 = tic;
+        end
+        counter = counter+1;
     end
-    drawnow;
 end
