7bc2d123a6809ef303332789bbf5dd90d58a591d
Stephen G. McGill <smcgill3@seas.upenn.edu>
2013-03-27 12:02:15 -0400

    Add a template for sending messages from webots over IPC and listening to them in another process

---
+++ Player/listen_zmq.lua
@@ -0,0 +1,42 @@
+module(... or '', package.seeall)
+
+cwd = '.';
+local platform = os.getenv('PLATFORM') or '';
+if (string.find(platform,'webots')) then cwd = cwd .. '/Player';
+end
+
+local computer = os.getenv('COMPUTER') or '';
+if (string.find(computer, 'Darwin')) then
+  package.cpath = cwd .. '/Lib/?.so;' .. package.cpath;
+else
+  package.cpath = cwd .. '/Lib/?.so;' .. package.cpath;
+end
+
+package.path = cwd .. '/?.lua;' .. package.path;
+package.path = cwd .. '/Util/?.lua;' .. package.path;
+package.path = cwd .. '/Config/?.lua;' .. package.path;
+package.path = cwd .. '/Lib/?.lua;' .. package.path;
+package.path = cwd .. '/Dev/?.lua;' .. package.path;
+package.path = cwd .. '/Motion/?.lua;' .. package.path;
+package.path = cwd .. '/Motion/keyframes/?.lua;' .. package.path;
+package.path = cwd .. '/Motion/Walk/?.lua;' .. package.path;
+package.path = cwd .. '/Vision/?.lua;' .. package.path;
+package.path = cwd .. '/World/?.lua;' .. package.path;
+require 'Config'
+local simple_ipc = require'simple_ipc'
+local state_channels = {};
+for i=1,Config.game.nPlayers do
+  state_channels[i] = simple_ipc.setup_subscriber('state'..i)
+  state_channels[i].callback = function()
+    local state, has_more = state_channels[i]:receive()
+    print('Player '..i..' state',state)
+  end
+end
+local state_poll = simple_ipc.wait_on_channels( state_channels );
+local state_timeout = 1000; -- 1000 ms
+while true do
+  -- Wait on a particular robot state
+  -- local state, has_more = state_channel:receive();
+  -- print( state );
+  state_poll:poll( state_timeout );
+end
---
+++ Player/main_zmq.lua
@@ -0,0 +1,204 @@
+module(... or '', package.seeall)
+
+-- Get Platform for package path
+cwd = '.';
+local platform = os.getenv('PLATFORM') or '';
+if (string.find(platform,'webots')) then cwd = cwd .. '/Player';
+end
+
+-- Get Computer for Lib suffix
+local computer = os.getenv('COMPUTER') or '';
+if (string.find(computer, 'Darwin')) then
+  -- MacOS X uses .dylib:
+--  package.cpath = cwd .. '/Lib/?.dylib;' .. package.cpath;
+  package.cpath = cwd .. '/Lib/?.so;' .. package.cpath;
+else
+  package.cpath = cwd .. '/Lib/?.so;' .. package.cpath;
+end
+
+package.path = cwd .. '/?.lua;' .. package.path;
+package.path = cwd .. '/Util/?.lua;' .. package.path;
+package.path = cwd .. '/Config/?.lua;' .. package.path;
+package.path = cwd .. '/Lib/?.lua;' .. package.path;
+package.path = cwd .. '/Dev/?.lua;' .. package.path;
+package.path = cwd .. '/Motion/?.lua;' .. package.path;
+package.path = cwd .. '/Motion/keyframes/?.lua;' .. package.path;
+package.path = cwd .. '/Motion/Walk/?.lua;' .. package.path;
+package.path = cwd .. '/Vision/?.lua;' .. package.path;
+package.path = cwd .. '/World/?.lua;' .. package.path;
+
+require('unix')
+require('Config')
+require('shm')
+require('vector')
+require('vcm')
+require('gcm')
+require('wcm')
+require('mcm')
+require('Speak')
+require('getch')
+require('Body')
+require('Motion')
+--------------
+-- ZMQ Channel
+local simple_ipc = require'simple_ipc'
+local channel = 'state'..Config.game.playerID;
+print('Channel',channel)
+local state_channel = simple_ipc.setup_publisher(channel)
+--------------
+gcm.say_id();
+
+Motion.entry();
+
+darwin = false;
+webots = false;
+
+-- Enable OP specific 
+if(Config.platform.name == 'OP') then
+  darwin = true;
+  --SJ: OP specific initialization posing (to prevent twisting)
+  Body.set_body_hardness(0.3);
+  Body.set_actuator_command(Config.stance.initangle)
+  unix.usleep(1E6*0.5);
+  Body.set_body_hardness(0);
+  Body.set_lleg_hardness({0.2,0.6,0,0,0,0});
+  Body.set_rleg_hardness({0.2,0.6,0,0,0,0});
+end
+
+-- Enable Webots specific
+if (string.find(Config.platform.name,'Webots')) then
+  webots = true;
+end
+
+init = false;
+calibrating = false;
+ready = false;
+if( webots or darwin) then
+  ready = true;
+end
+
+smindex = 0;
+initToggle = true;
+
+-- main loop
+count = 0;
+lcount = 0;
+tUpdate = unix.time();
+
+function update()
+  count = count + 1;
+  --Update battery info
+  wcm.set_robot_battery_level(Body.get_battery_level());
+  vcm.set_camera_teambroadcast(1); --Turn on wireless team broadcast
+
+  if (not init)  then
+    if (calibrating) then
+      if (Body.calibrate(count)) then
+        Speak.talk('Calibration done');
+        calibrating = false;
+        ready = true;
+      end
+    elseif (ready) then
+      -- initialize state machines
+      package.path = cwd..'/BodyFSM/'..Config.fsm.body[smindex+1]..'/?.lua;'..package.path;
+      package.path = cwd..'/HeadFSM/'..Config.fsm.head[smindex+1]..'/?.lua;'..package.path;
+      package.path = cwd..'/GameFSM/'..Config.fsm.game..'/?.lua;'..package.path;
+      require('BodyFSM')
+      require('HeadFSM')
+      require('GameFSM')
+
+      BodyFSM.entry();
+      HeadFSM.entry();
+      GameFSM.entry();
+
+      if( webots ) then
+        --BodyFSM.sm:add_event('button');
+        GameFSM.sm:set_state('gamePlaying');
+      end
+
+      init = true;
+    else
+      if (count % 20 == 0) then
+--      if (Body.get_change_state() == 1) then
+	  if true then
+          Speak.talk('Calibrating');
+          calibrating = true;
+        elseif (Body.get_change_role() == 1) then
+          smindex = (smindex + 1) % #Config.fsm.body;
+        end
+      end
+
+      -- toggle state indicator
+      if (count % 100 == 0) then
+        initToggle = not initToggle;
+        if (initToggle) then
+          Body.set_indicator_state({1,1,1}); 
+        else
+          Body.set_indicator_state({0,0,0});
+        end
+      end
+    end
+
+  else
+    -- update state machines 
+    GameFSM.update();
+    BodyFSM.update();
+    HeadFSM.update();
+    ---------------
+    -- FSM override
+    local my_state = "";
+    local pose = wcm.get_pose();
+    my_state = my_state..string.format("[%.3f %.3f %.3f]", pose.x, pose.y, pose.a );
+    local ball = wcm.get_ball();
+    my_state = my_state..string.format("[%.3f %.3f]",ball.x,ball.y,ball.a);
+    state_channel:send(my_state);
+    ---------------
+    Motion.update();
+    Body.update();
+  end
+
+  local dcount = 50;
+  if (count % 50 == 0) then
+--    print('fps: '..(50 / (unix.time() - tUpdate)));
+    tUpdate = unix.time();
+
+    -- update battery indicator
+    Body.set_indicator_batteryLevel(Body.get_battery_level());
+  end
+  
+  -- check if the last update completed without errors
+  lcount = lcount + 1;
+  if (count ~= lcount) then
+    print('count: '..count)
+    print('lcount: '..lcount)
+    Speak.talk('missed cycle');
+    lcount = count;
+  end
+end
+
+-- if using Webots simulator just run update
+if (webots) then
+  require('cognition');
+  cognition.entry();
+
+  -- set game state to Playing
+  gcm.set_game_state(3);
+
+  while (true) do
+    -- update cognitive process
+    cognition.update();
+    -- update motion process
+    update();
+
+    io.stdout:flush();
+  end
+
+end
+
+if( darwin ) then
+  local tDelay = 0.005 * 1E6; -- Loop every 5ms
+  while 1 do
+    update();
+    unix.usleep(tDelay);
+  end
+end
